e0775c903e5f31127f2a55f7c2cff14ac63bfa75
Ando <65276708+andostronaut@users.noreply.github.com>
2024-07-19 23:26:07 +0300

    feat: use typeform for join us

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -19,7 +19,7 @@ export const HeroSection = () => {
 
   return (
     <section className="container w-full lg:min-h-[65vh] md:min-h-[65vh]">
-      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
+      <div className="relative grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32 z-50">
         <div className="text-center space-y-8">
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
             <h1>
@@ -35,7 +35,10 @@ export const HeroSection = () => {
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
+            <Button
+              className="cursor-pointer w-5/6 md:w-1/4 font-bold group/arrow"
+              onClick={() => window.open(DATA.typeform, '__blank')}
+            >
               Join us
               <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
             </Button>
@@ -44,7 +47,7 @@ export const HeroSection = () => {
       </div>
 
       <Particles
-        className="absolute inset-0"
+        className="absolute inset-0 z-0"
         quantity={100}
         ease={80}
         color={color}
