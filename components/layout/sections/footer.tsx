55a75808525e4eea5bd950e1884d9c784a0201ec
Ando <65276708+andostronaut@users.noreply.github.com>
2024-07-19 20:28:08 +0300

    feat: update to base theme and use new logo footer and navbar

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -1,17 +1,33 @@
-import { Separator } from '@/components/ui/separator'
-import { ChevronsDownIcon } from 'lucide-react'
+'use client'
+
 import Link from 'next/link'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+
+import { Separator } from '@/components/ui/separator'
+
+import { getCurrentYear } from '@/lib/utils'
 
 export const FooterSection = () => {
+  const { theme } = useTheme()
+
   return (
     <footer id="footer" className="container py-24 sm:py-32">
       <div className="p-10 bg-card border border-secondary rounded-2xl">
         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
-
-              <h3 className="text-2xl">Shadcn</h3>
+              <Image
+                src={
+                  theme === 'light'
+                    ? '/images/logo-dark.png'
+                    : '/images/logo-light.png'
+                }
+                width={100}
+                height={100}
+                alt="Kinotio"
+                className="ml-2"
+              />
             </Link>
           </div>
 
@@ -103,13 +119,13 @@ export const FooterSection = () => {
         <Separator className="my-6" />
         <section className="">
           <h3 className="">
-            &copy; 2024 Designed and developed by
+            {` © ${getCurrentYear()} `}
             <Link
               target="_blank"
-              href="https://github.com/leoMirandaa"
+              href="https://github.com/kinotio"
               className="text-primary transition-all border-primary hover:border-b-2 ml-1"
             >
-              Leo Miranda
+              Kinotio
             </Link>
           </h3>
         </section>
