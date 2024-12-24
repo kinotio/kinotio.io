d6029f7d7419ae3df6ec449a86f116ea8959cb1c
Ando <hi@andostronaut.com>
2024-12-24 11:19:28 +0300

    feat: remove unused deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import '@/app/globals.css'
-import '@devnomic/marquee/dist/index.css'
 
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
@@ -72,7 +71,6 @@ const RootLayout = ({
         <div className="min-h-screen bg-gradient-to-br from-black via-[#1a1a2e] to-[#16213e] relative overflow-hidden">
           <div className="absolute inset-0 bg-[url('/noise.png')] opacity-5 mix-blend-soft-light"></div>
           <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black opacity-20"></div>
-
           <Header />
           <main className="container">{children}</main>
           <Footer />
