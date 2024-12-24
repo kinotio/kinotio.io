be8d480d0011de40b30a42807d84e7b16aa6dc76
Ando <hi@andostronaut.com>
2024-12-24 09:56:17 +0300

    feat: remove translation locales

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,65 @@ import '@/app/globals.css'
 import '@devnomic/marquee/dist/index.css'
 
 import { Inter } from 'next/font/google'
-import { cookies } from 'next/headers'
 import Script from 'next/script'
+import type { Metadata } from 'next'
+
+import { Header } from '@/components/header'
+import { Footer } from '@/components/footer'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
 
 import { cn } from '@/lib/utils'
+import { DATA } from '@/data'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    metadataBase: new URL(DATA.url),
+    title: {
+      default: DATA.name,
+      template: `%s | ${DATA.name}`,
+    },
+    description: DATA.description,
+    keywords: DATA.keywords,
+    openGraph: {
+      title: `${DATA.name}`,
+      description: DATA.description,
+      url: DATA.url,
+      siteName: `${DATA.name}`,
+      locale: 'en',
+      type: 'website',
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
+    },
+    twitter: {
+      title: `${DATA.name}`,
+      card: 'summary_large_image',
+    },
+    verification: {
+      google: '',
+      yandex: '',
+    },
+  }
+}
 
 const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) => {
-  const locale = cookies().get('Next-Locale')?.value || 'en'
-
   return (
-    <html lang={locale} suppressHydrationWarning>
+    <html suppressHydrationWarning>
       <Script
         async
         src="https://analytics.kinotio.io/script.js"
@@ -28,7 +71,25 @@ const RootLayout = ({
         src="https://kinotio.instatus.com/en/6f8e5fad/widget/script.js"
       />
       <body className={cn('min-h-screen bg-background', inter.className)}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div>
+            <div className="min-h-screen bg-gradient-to-br from-black via-[#1a1a2e] to-[#16213e] relative overflow-hidden">
+              <div className="absolute inset-0 bg-[url('/noise.png')] opacity-5 mix-blend-soft-light"></div>
+              <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black opacity-20"></div>
+
+              <Header />
+              <main className="container">{children}</main>
+              <Footer />
+            </div>
+
+            <Toaster />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
